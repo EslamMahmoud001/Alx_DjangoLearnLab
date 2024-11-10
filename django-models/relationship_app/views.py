3f3c5ab9264aa9c0f07c8ed107bb073c8e300980
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-11-10 23:33:17 +0200

    week 10

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from django.views.generic import DetailView
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
