1e8b13b7b510a1e0e9e8e2ae3e1eaa81198c1a42
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-11-10 23:36:53 +0200

    week 10

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
+
 
 # User Registration View
 def register(request):
@@ -31,3 +34,34 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html', "library", "from .models import Library"
     context_object_name = 'library'
+    
+
+# Function to check if the user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Function to check if the user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Function to check if the user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+# Admin View: Only accessible by users with 'Admin' role
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome to the Admin View!")
+
+
+# Librarian View: Only accessible by users with 'Librarian' role
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome to the Librarian View!")
+
+
+# Member View: Only accessible by users with 'Member' role
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome to the Member View!")
