71bfe75cf7bf3e5b4a26a5cfaab4faa2f9afe63a
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-11-10 23:26:40 +0200

    week 10

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # Map the URL to the function-based view
+    path('books/', views.list_books, name='list_books'),  # FBV to list books
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV to show library details
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
 # Create your views here.
 
@@ -7,3 +9,8 @@ def list_books(request):
     books = Book.objects.all()  # Get all books from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html', "library", "from .models import Library"
+    context_object_name = 'library'
