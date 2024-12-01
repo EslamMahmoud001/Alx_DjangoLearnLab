ef63f3021f0f64a958456c950b11d28c38502f52
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-12-01 22:22:35 +0200

    week13

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from django_filters import rest_framework
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
@@ -17,7 +17,7 @@ class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]  # Allows read-only access to unauthenticated users
     
     # Enable filtering, searching, and ordering
-    filter_backends = (rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_backends = (rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     
     # Filtering options: Allow filtering by title, author, and publication_year
     filterset_fields = ['title', 'author__name', 'publication_year']
