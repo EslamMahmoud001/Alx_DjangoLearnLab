156eb5ced1d66e49bb3d8f0c216704b1ca8c0ae5
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-12-01 21:56:08 +0200

    week 13 deleted

--- advanced_api_project/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BookListView, BookDetailView
-
-urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),  # List all books, create a new book
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, or delete a book
-]
