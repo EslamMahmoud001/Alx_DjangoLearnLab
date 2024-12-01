e7489a9a046c6cd85c0ecfbc57fc78615e8d706d
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-12-01 21:24:30 +0200

    week 12

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Query the Book model
     serializer_class = BookSerializer  # Use the BookSerializer to format the data
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()  # Queryset for retrieving, updating, and deleting books
+    serializer_class = BookSerializer  # Serializer for formatting book data
