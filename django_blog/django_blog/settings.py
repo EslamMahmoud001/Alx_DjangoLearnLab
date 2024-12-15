4ba48d100528af187bd26212eb12d69bd0f5b5d1
EslamMahmoud001 <Eslam.Mahmoud767@gmail.com>
2024-12-15 17:17:30 +0200

    week 14

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',  # Set to the host of your database server
+        'PORT': '5432',       # Default port for PostgreSQL
     }
 }
 
